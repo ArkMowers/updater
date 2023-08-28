da9e34e0c2ad2123559bccb79679f2c05a4d39c4
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-08-28 16:09:00 +0800

    下载文件

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import json
 import requests
 import htmllistparse
 from hash import hash
+from fnmatch import fnmatch
 
 
 default_config = {
@@ -14,9 +15,9 @@ default_config = {
     "ignore": [
         "*.conf",
         "*.json",
-        "tmp/",
-        "screenshot/",
-        "adb_buildin/",
+        "tmp/**",
+        "screenshot/**",
+        "adb_buildin/**",
     ],
     "install_dir": "",
 }
@@ -90,7 +91,6 @@ def fetch_version_details(mirror, versions):
             r = requests.get(url)
             pub_time = r.json()["time"]
         except Exception as e:
-            print(e)
             continue
         result.append(
             {
@@ -105,41 +105,69 @@ def fetch_version_details(mirror, versions):
 new_list = []
 replace_list = []
 remove_list = []
+ignore_list = []
 
 
-def prepare_to_install(path, new_hash):
+def prepare_to_install(path, new_hash, pattern_list):
     global new_list
     global replace_list
     global remove_list
+    global ignore_list
+    new_list = []
+    replace_list = []
+    remove_list = []
+    ignore_list = []
+    for pattern in pattern_list:
+        for file in path.glob(pattern):
+            ignore_list.append(str(file)[len(str(path)) + 1 :])
     old_hash = hash(path)
     for f, h in new_hash.items():
         if f in old_hash:
-            if old_hash[f] != h:
+            if old_hash[f] != h and f not in ignore_list:
+                print(f)
+                print(old_hash[f])
+                print(h)
                 replace_list.append(f)
-        else:
+        elif f not in ignore_list:
             new_list.append(f)
     for f, h in old_hash.items():
-        if f not in new_hash:
+        if f not in new_hash and f not in ignore_list:
             remove_list.append(f)
-    print(
-        f"new: {len(new_list)}, replace: {len(replace_list)}, remove: {len(remove_list)}"
-    )
-    print("new:")
-    print(new_list)
-    print("replace:")
-    print(replace_list)
-    print("remove:")
-    print(remove_list)
 
 
-global versions
+version_name = ""
+
+
+def download_single_file():
+    mirror = conf["mirror"]
+    if not mirror.endswith("/"):
+        mirror += "/"
+    global new_list
+    global replace_list
+    global remove_list
+    if remove_list:
+        subpath = remove_list.pop()
+        path = pathlib.Path(conf["install_dir"]) / "mower" / subpath
+        path.unlink()
+        return subpath
+    elif new_list:
+        subpath = new_list.pop()
+    elif replace_list:
+        subpath = replace_list.pop()
+    else:
+        return None
+    url = f"{mirror}{version_name}/{subpath}"
+    print(url)
+    r = requests.get(url)
+    path = pathlib.Path(conf["install_dir"]) / "mower" / subpath
+    path.parent.mkdir(exist_ok=True, parents=True)
+    with path.open("wb") as f:
+        f.write(r.content)
+    return subpath
 
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
-
     conf["mirror"] = values["-mirror-"]
     conf["ignore"] = [l for l in values["-ignore-"].splitlines() if l.strip()]
     conf["install_dir"] = values["install-dir"]
@@ -163,28 +191,56 @@ while True:
         else:
             sg.PopupError(values["-connect-mirror-"]["msg"])
     elif event == "-version-details-":
-        global versions
         versions = values["-version-details-"]
         window["versions"].update(values=[v["display_name"] for v in versions])
         window["status"].update("已获取版本列表")
     elif event == "开始安装":
         window["status"].update("正在安装……")
         version_display_name = values["versions"][0]
-        version_hash_list = next(
-            i["hash"] for i in versions if i["display_name"] == version_display_name
-        )
+        version = next(i for i in versions if i["display_name"] == version_display_name)
+        version_hash_list = version["hash"]
+        version_name = version["version"]
         path = pathlib.Path(conf["install_dir"]) / "mower"
         path.mkdir(exist_ok=True, parents=True)
         window.perform_long_operation(
-            lambda: prepare_to_install(path, version_hash_list),
+            lambda: prepare_to_install(path, version_hash_list, conf["ignore"]),
             "-calc-hash-",
         )
     elif event == "-calc-hash-":
-        window["status"].update("安装完成")
+        begin_install = sg.popup_scrolled(
+            f"预计新增{len(new_list)}个文件：\n"
+            + "\n".join(new_list)
+            + f"\n\n替换{len(replace_list)}个文件：\n"
+            + "\n".join(replace_list)
+            + f"\n\n删除{len(remove_list)}个文件：\n"
+            + "\n".join(remove_list)
+            + "\n\n是否继续安装？",
+            yes_no=True,
+            title="安装确认",
+        )
+        if begin_install == "Yes":
+            window.perform_long_operation(
+                lambda: download_single_file(),
+                "-install-single-",
+            )
+        else:
+            window["status"].update("安装已取消")
+    elif event == "-install-single-":
+        subpath = values["-install-single-"]
+        print(subpath)
+        if subpath:
+            window["status"].update(
+                f"已处理{subpath}，剩余{len(remove_list) + len(replace_list) + len(new_list)}个文件……"
+            )
+            window.perform_long_operation(
+                lambda: download_single_file(),
+                "-install-single-",
+            )
+        else:
+            window["status"].update("安装完成！")
 
 
 window.close()
 
-print(conf)
 with conf_path.open("w") as f:
     json.dump(conf, f)
